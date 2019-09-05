ee7abb707dd066e9f6340a86018ba953d08d0ef9
Matthew Hiles <matthew.hiles@gmail.com>
2019-09-05 08:07:40 -0400

    IT TOTALLY WORKS

--- main.c
+++ main.c
@@ -32,27 +32,21 @@
 #define SAMPLE_MIN (PWM_MIN_US/SAMPLE_PERIOD_US)
 #define SAMPLE_MAX (PWM_MAX_US/SAMPLE_PERIOD_US)
 
-#define FLOAT2PWM(in) (((unsigned int) in)*(SAMPLE_RANGE)+SAMPLE_MIN)
-
-#define FIX162PWM(in) ((unsigned int)fix16_to_float(fix16_add(fix16_mul(in,fix16_from_int(SAMPLE_RANGE)),fix16_from_int(SAMPLE_MIN))))
-#define PWM2FIX16(in) (fix16_div(fix16_sub(fix16_from_int(in),fix16_from_int(((SAMPLE_RANGE/2)+SAMPLE_MIN))), (SAMPLE_RANGE/2)))
+#define FIX162PWM(in) ((unsigned int)fix16_to_float(fix16_add(fix16_mul(in,fix16_from_int(SAMPLE_RANGE/2)),fix16_from_int(SAMPLE_MIN+SAMPLE_RANGE/2))))
+#define PWM2FIX16(in) (fix16_div(fix16_sub(fix16_from_int(in),fix16_from_int(((SAMPLE_RANGE/2)+SAMPLE_MIN))), fix16_from_int(SAMPLE_RANGE/2)))
 
 #define MAX(_v,_max) (_v > _max ? _max : _v)
 #define MIN(_v,_min) (_v < _min ? _min : _v)
 #define MINMAX(_v, _min, _max) (MAX(MIN(_v, _min),_max))
 
-enum pwm_buffers {
-	XBUF,
-	YBUF,
-	ABUF
-};
+#define XBUF 0
+#define YBUF 1
+#define ABUF 2
 
-enum pwm_outputs {
-	M1,
-	M2,
-	M3,
-	M4
-};
+#define M1 0
+#define M2 1
+#define M3 2
+#define M4 3
 
 struct pwm {
 	unsigned int buf;   // the current complete width calculated from wpm
@@ -96,7 +90,7 @@ int main()
 	
 	// pwm buffers
 	struct pwm inputs[3] = {
-		 {((SAMPLE_RANGE)+SAMPLE_MIN),0,0},
+		 {((SAMPLE_RANGE/2)+SAMPLE_MIN),0,0},
 		 {((SAMPLE_RANGE/2)+SAMPLE_MIN),0,0},
 		 {((SAMPLE_RANGE/2)+SAMPLE_MIN),0,0}
 	};
@@ -118,11 +112,10 @@ int main()
 
 		// store P1IN value so it can't change under us / we're looking at a single slice in time
 		p1inbuf = P1IN;
-/*
 		get_pwm_input(&inputs[XBUF], p1inbuf & X_AXIS ? 1 : 0);
 		get_pwm_input(&inputs[YBUF], p1inbuf & Y_AXIS ? 1 : 0);
 		get_pwm_input(&inputs[ABUF], p1inbuf & A_AXIS ? 1 : 0);
-*/
+
 		/*
 		 * The value in inputs[].buf is the length of the pwm pulse. 150 (1.5ms) is neutral
 		 * 50 (0.5ms) is minimum value and 250 (2.5ms) is maximum value
@@ -140,33 +133,24 @@ int main()
 
 		 */
 
-		// If this trig garbage takes to long to complete we can opt to do it only every
-		// other loop or 1/10th a loop (or up the DCO clock)
-
-		// do trig here to reconcile X, Y, and A axes
-		// LMAO, I just realized I know how to reconsile X and Y but A is 🤷
-		// Okay, internet to the rescue... found some equations to crib
-		// if (cycles++ % (1<<15) == 0 ) {
-		// Don't do all this expensive floating point math unless one of the inputs has changed
-		// Doesn't this mean that sending a lot of commands will diminish performance???
-		if (recompute_flag || 1) {
+		// if input PWM has changed, recompute our outputs
+		if (recompute_flag) {
 			fix16_t x = PWM2FIX16(inputs[XBUF].buf);
 			fix16_t y = PWM2FIX16(inputs[YBUF].buf);
-			fix16_t a = PWM2FIX16(inputs[ABUF].buf);
-
+			fix16_t v_theta = PWM2FIX16(inputs[ABUF].buf);
 			
-			// derive theta, magnitude, and rotation
-
-			fix16_t theta_d = fix16_atan2(x,y);
+			// derive theta (angle of desired movement), and magnitude (v_d)
+			fix16_t theta_d = fix16_atan2(y,x);
 			fix16_t v_d = fix16_sqrt(fix16_add(fix16_mul(x,x), fix16_mul(y,y)));
-			fix16_t v_theta = fix16_from_int(0);
 
-			fix16_t trig_arg = fix16_sub(THREE_PI_DIV_4, theta_d);
-
-			outputs[M1].width = FIX162PWM(fix16_sub(fix16_mul(v_d, fix16_sin(trig_arg)), v_theta));
-			outputs[M2].width = FIX162PWM(fix16_add(fix16_mul(v_d, fix16_cos(trig_arg)), v_theta));
-			outputs[M3].width = FIX162PWM(fix16_sub(fix16_mul(v_d, fix16_cos(trig_arg)), v_theta));
-			outputs[M4].width = FIX162PWM(fix16_add(fix16_mul(v_d, fix16_sin(trig_arg)), v_theta));
+			fix16_t trig_arg = fix16_sub(PI_DIV_4, theta_d);
+			fix16_t vdcos = fix16_mul(v_d, fix16_cos(trig_arg));
+			fix16_t vdsin = fix16_mul(v_d, fix16_sin(trig_arg));
+			
+			outputs[M1].width = FIX162PWM(fix16_sub(vdsin, v_theta));
+			outputs[M2].width = FIX162PWM(fix16_add(vdcos, v_theta));
+			outputs[M3].width = FIX162PWM(fix16_sub(vdcos, v_theta));
+			outputs[M4].width = FIX162PWM(fix16_add(vdsin, v_theta));
 
 			recompute_flag = 0;
 		}
@@ -193,11 +177,13 @@ inline void get_pwm_input(struct pwm *input, unsigned int cur) {
 		if (cur) {
 			input->count = 1;
 		} else {
-			recompute_flag = 1;
-			input->buf = input->count;
-			if input->buf < SAMPLE_MIN
+			if (input->buf != input->count) {
+				recompute_flag = 1;
+				input->buf = input->count;
+			}
+			if (input->buf < SAMPLE_MIN)
 				input->buf = SAMPLE_MIN;
-			if input->buf > SAMPLE_MAX
+			if (input->buf > SAMPLE_MAX)
 				input->buf = SAMPLE_MAX;
 		}
 	}
@@ -219,4 +205,3 @@ __interrupt_vec(TIMER0_A0_VECTOR) void timerisr()
 {
 	__low_power_mode_off_on_exit();
 }
-
